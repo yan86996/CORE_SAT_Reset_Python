b14fc9ec73583248d22be15e17b15124890d1ac1
yan86996 <149843661+yan86996@users.noreply.github.com>
2025-02-02 16:52:46 -0800

    Update rand_date_gen.py

--- reset_strategies/rand_date_gen.py
+++ reset_strategies/rand_date_gen.py
@@ -1,9 +1,20 @@
+import datetime as dt
 import numpy as np
-import datetime
-start = datetime.date(2016,7,27)
-arr = np.array([start + datetime.timedelta(days=1*i) for i in xrange(600)])
-np.random.shuffle(arr)
-rand = arr[:300]
-RAND_DATES = np.sort(rand)
-#RAND_DATES = np.sort(np.append(rand, rand + datetime.timedelta(days=1)))
-print RAND_DATES
+
+# extracted by extract_dev_ID.py
+rand_dates_Baseline = np.array([dt.date(2024, 11, 6),  dt.date(2024, 11, 8),
+                       dt.date(2024, 11, 21),  dt.date(2024, 11, 30),                       
+                       ])
+
+rand_dates_G36 = np.array([dt.date(2024, 11, 23),   dt.date(2024, 12, 6),
+                           dt.date(2024, 12, 9),   dt.date(2024, 12, 10),
+                           dt.date(2024, 12, 12),  dt.date(2024, 12, 13),                           
+                            ])
+
+# TO CHANGE
+start_date = dt.date(2025, 2, 1)
+end_date = dt.date(2025, 3, 31)
+# Generate continuous dates
+num_days = (end_date - start_date).days + 1
+rand_dates_CORE = np.array([start_date + dt.timedelta(days=i) for i in range(num_days)])
+                       
