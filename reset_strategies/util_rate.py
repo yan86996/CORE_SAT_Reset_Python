c4b2d2e5a7788ce583b9b63c36f5a7848d40299b
yan86996 <149843661+yan86996@users.noreply.github.com>
2025-05-04 12:22:11 -0700

    Update util_rate.py

--- reset_strategies/util_rate.py
+++ reset_strategies/util_rate.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 
 def electricity_price(datetime_obj):
-    # Define the price period based on PG&E schedule
-    month_rate = {1: {2025: 0.081,},  2: {2025: 0.066,},  3: {2025: 0.074,},
-                  4: {2025: 0.069,},  5: {2025: 0.074,},  6: {2025: 0.119,},
-                  7: {2025: 0.127,},  8: {2025: 0.124,},  9: {2025: 0.136,},
-                 10: {2025: 0.106,}, 11: {2025: 0.075,}, 12: {2025: 1.013,},
+    co2_cost = 267  # 267 $/ton CO2
+    co2_elec_cost = (289/907185) * co2_cost  # 289g CO2e/kWh for electricity, 1 ton=907185g
+    month_rate = {1: {2025:0.081+co2_elec_cost,},  2: {2025:0.066+co2_elec_cost,},  3: {2025:0.074+co2_elec_cost,},
+                  4: {2025:0.069+co2_elec_cost,},  5: {2025:0.074+co2_elec_cost,},  6: {2025:0.119+co2_elec_cost,},
+                  7: {2025:0.127+co2_elec_cost,},  8: {2025:0.124+co2_elec_cost,},  9: {2025:0.136+co2_elec_cost,},
+                 10: {2025:0.106+co2_elec_cost,}, 11: {2025:0.075+co2_elec_cost,}, 12: {2025:1.013+co2_elec_cost,},
                  }
      
     dt = datetime_obj
@@ -14,11 +15,14 @@ def electricity_price(datetime_obj):
     return energy_charge
 
 def steam_price(datetime_obj):
-    # Define the price period based on PG&E schedule
-    month_rate = {1: {2025: 51.972/1000,},  2: {2025: 51.972/1000,},  3: {2025: 51.972/1000,},
-                  4: {2025: 51.972/1000,},  5: {2025: 51.972/1000,},  6: {2025: 51.972/1000,},
-                  7: {2025: 51.972/1000,},  8: {2025: 51.972/1000,},  9: {2025: 51.972/1000,},
-                 10: {2025: 51.972/1000,}, 11: {2025: 51.972/1000,}, 12: {2025: 51.972/1000,},
+    ult_steam = 51.972/1000  # $/lb
+    co2_cost = 267  # 267 $/ton CO2
+    co2_steam_cost = (53.6/1000) /907.185 * co2_cost  # 53.6 kg CO2e/mlbs for steam
+    
+    month_rate = {1: {2025: ult_steam+co2_steam_cost,},  2: {2025: ult_steam+co2_steam_cost,},  3: {2025: ult_steam+co2_steam_cost,},
+                  4: {2025: ult_steam+co2_steam_cost,},  5: {2025: ult_steam+co2_steam_cost,},  6: {2025: ult_steam+co2_steam_cost,},
+                  7: {2025: ult_steam+co2_steam_cost,},  8: {2025: ult_steam+co2_steam_cost,},  9: {2025: ult_steam+co2_steam_cost,},
+                 10: {2025: ult_steam+co2_steam_cost,}, 11: {2025: ult_steam+co2_steam_cost,}, 12: {2025: ult_steam+co2_steam_cost,},
                  }
      
     dt = datetime_obj
